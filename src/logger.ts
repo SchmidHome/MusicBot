196a11edea434113493a0cc41414e7638a3ed8f7
Emergency1999 <johannes@schmid-company.de>
2022-12-16 22:34:48 +0100

    updated logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,5 @@
 import chalk, { Chalk } from "chalk"
+import { appendFile, fstat, writeFile } from "fs"
 import { Logger } from "ts-log"
 
 function padCenter(str: string, len: number) {
@@ -7,18 +8,10 @@ function padCenter(str: string, len: number) {
 
 export class ConsoleLogger implements Logger {
     public static colors: Chalk[] = [
-        // chalk.blueBright,
-        // chalk.greenBright,
-        // chalk.yellowBright,
-        // chalk.magentaBright,
-        // chalk.cyanBright,
-        // chalk.whiteBright,
         chalk.cyan,
         chalk.magenta,
         chalk.green,
         chalk.black,
-        // chalk.blackBright,
-        // chalk.redBright,
         chalk.red,
         chalk.yellow,
         chalk.blue,
@@ -26,11 +19,19 @@ export class ConsoleLogger implements Logger {
     ]
     static maxNameLength = 0
 
-    color: Chalk
+    public getTime(): string {
+        // HH:MM:SS.mmm
+        return new Date().toISOString().substring(11, 23)
+    }
+
+    public readonly filename: string
+    public readonly color: Chalk
     public constructor(private name: string) {
         // select a foreground color and remove it from the list
         this.color = ConsoleLogger.colors.shift() || chalk.whiteBright
         ConsoleLogger.maxNameLength = Math.max(ConsoleLogger.maxNameLength, name.length)
+        this.filename = `logs/${name}.ans`
+        writeFile(this.filename, "\n\nSTARTED " + this.getTime() + "\n", () => { })
     }
 
     public trace(message?: any, ...optionalParams: any[]): void {
@@ -40,7 +41,6 @@ export class ConsoleLogger implements Logger {
     public debug(message?: any, ...optionalParams: any[]): void {
         this.append("DEBUG", chalk.cyan, chalk.white, message, ...optionalParams)
     }
-
 
     public log: (message?: any, ...optionalParams: any[]) => void = this.info;
     public info(message?: any, ...optionalParams: any[]): void {
@@ -56,8 +56,11 @@ export class ConsoleLogger implements Logger {
     }
 
     private append(type: string, colorType: Chalk, colorText: Chalk, message: string, ...optionalParams: any[]) {
-        const time = new Date().toTimeString().split(" ")[0]
-        console.log(`[${time}] [${colorType(type)}] [${this.color(padCenter(this.name, ConsoleLogger.maxNameLength))}] ${colorText(message)}`, ...optionalParams)
+        const msg = `[${this.getTime()}] [${colorType(type)}] [${this.color(padCenter(this.name, ConsoleLogger.maxNameLength))}] ${colorText(message)}`
+        console.log(msg, ...optionalParams)
+        const params = JSON.stringify(optionalParams)
+        appendFile(this.filename, msg + (
+            params === "[]" ? "" : " " + params
+        ) + "\n", () => { })
     }
 }
-
