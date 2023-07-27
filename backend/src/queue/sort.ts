c5e8708321529c8eddd3a9fa54d4173cb7c4eacf
Johannes Schmid <johannes@schmid-company.de>
2023-07-28 01:46:01 +0200

    added sortQueue

--- backend/src/queue/sort.ts
+++ backend/src/queue/sort.ts
@@ -1,6 +1,24 @@
 import { getSong } from "../spotify/songCache";
-import { getNext, getPlaying, getQueued } from "./getter";
-import { setPlayStartTime } from "./setter";
+import { getNew, getNext, getPlaying, getQueued } from "./getter";
+import { setPlayStartTime, setPosition } from "./setter";
+
+export async function sortQueue() {
+  let next = await getNext();
+  let elements = [...(await getQueued()), ...(await getNew())];
+  let startPos = next ? 1 : 0;
+
+  elements = elements.sort((a, b) => {
+    if (typeof a.pos === "number" && typeof b.pos === "number") {
+      return a.pos - b.pos;
+    } else if (typeof a.pos === "number") {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  await Promise.all(elements.map((e, i) => setPosition(e._id, startPos + i)));
+}
 
 export async function updateTime() {
   const faderTime = 1000 * 12;
