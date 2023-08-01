cb6357798ace825fb298c69685e3fcbb86a0fa97
Emergency1999 <johannes@schmid-company.de>
2023-08-01 08:37:37 +0200

    added mutex for sort

--- backend/src/queue/sort.ts
+++ backend/src/queue/sort.ts
@@ -1,12 +1,17 @@
 import { ObjectId, WithId } from "mongodb";
 import { getSong } from "../spotify/songCache";
-import { getNew, getNext, getPlaying, getQueued, getFullQueue } from "./getter";
+import { getNext, getPlaying, getQueued, getFullQueue } from "./getter";
 import { setPlayStartTime, setPosition } from "./setter";
 import { QueueElement } from "./types";
-import { number } from "zod";
+import { Mutex } from "async-mutex";
 
-export async function sortQueue(elements?: WithId<QueueElement>[]) {
-  //TODO use semaphore
+const sortMutex = new Mutex();
+export async function sortQueue() {
+  return sortMutex.runExclusive(async () => {
+    await _sortQueue();
+  });
+}
+async function _sortQueue(elements?: WithId<QueueElement>[]) {
   if (!elements) elements = await getFullQueue();
 
   elements = elements.map((e) => {
@@ -35,40 +40,44 @@ export async function sortQueue(elements?: WithId<QueueElement>[]) {
 }
 
 export async function resortQueue(_id: ObjectId, posChange: number) {
-  let elements = await getFullQueue();
+  return sortMutex.runExclusive(async () => {
+    let elements = await getFullQueue();
 
-  if (posChange > 0) posChange += 0.5;
-  if (posChange < 0) posChange -= 0.5;
+    if (posChange > 0) posChange += 0.5;
+    if (posChange < 0) posChange -= 0.5;
 
-  const changeElement = elements.find((e) => e._id.equals(_id));
-  if (!changeElement || !changeElement.pos) return false;
-  changeElement.pos += posChange;
+    const changeElement = elements.find((e) => e._id.equals(_id));
+    if (!changeElement || !changeElement.pos) return false;
+    changeElement.pos += posChange;
 
-  await sortQueue(elements);
-  return true;
+    await _sortQueue(elements);
+    return true;
+  });
 }
 
 export async function updateTime() {
-  const faderTime = 1000 * 12;
+  return sortMutex.runExclusive(async () => {
+    const faderTime = 1000 * 12;
 
-  let playing = await getPlaying();
-  if (!playing) return;
-  if (!playing.playStartTime) return;
+    let playing = await getPlaying();
+    if (!playing) return;
+    if (!playing.playStartTime) return;
 
-  let time =
-    playing.playStartTime.getTime() +
-    (await getSong(playing.songUri)).duration_ms +
-    faderTime / 2;
+    let time =
+      playing.playStartTime.getTime() +
+      (await getSong(playing.songUri)).duration_ms +
+      faderTime / 2;
 
-  let next = await getNext();
-  if (next) {
-    await setPlayStartTime(next._id, new Date(time - faderTime));
-    time += (await getSong(next.songUri)).duration_ms - faderTime;
-  }
+    let next = await getNext();
+    if (next) {
+      await setPlayStartTime(next._id, new Date(time - faderTime));
+      time += (await getSong(next.songUri)).duration_ms - faderTime;
+    }
 
-  let queue = await getQueued();
-  for (let element of queue) {
-    await setPlayStartTime(element._id, new Date(time - faderTime));
-    time += (await getSong(element.songUri)).duration_ms - faderTime;
-  }
+    let queue = await getQueued();
+    for (let element of queue) {
+      await setPlayStartTime(element._id, new Date(time - faderTime));
+      time += (await getSong(element.songUri)).duration_ms - faderTime;
+    }
+  });
 }
